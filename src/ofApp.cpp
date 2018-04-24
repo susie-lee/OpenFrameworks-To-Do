6f5d2bb058c481184a84c08bee1825b6f729f628
Susie Lee <susiel2@illinois.edu>
2018-04-24 04:05:57 -0500

    FINALLY GOT LIGHTING TO WORK

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -17,33 +17,22 @@ void toDoList::keyPressed(int key) {
 
 void toDoList::draw() {
   flower.setPosition(ofGetWidth()/2, (float)ofGetHeight() * 0.75 , 0);
-////    ofEnableDepthTest();
-////    ofEnableLighting();
-//    glDisable(GL_COLOR_MATERIAL);
-////    ofSetColor(250,250,250);
-//    glEnable(GL_LIGHTING);
-//    glEnable(GL_NORMALIZE);
-//    glEnable(GL_LIGHT7);
-//    glDisable(GL_COLOR_MATERIAL);
+    ofEnableDepthTest();
+    ofEnableLighting();
+    glEnable(GL_LIGHTING);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_LIGHT0);
+    glDisable(GL_COLOR_MATERIAL);
 //
-//    ofPushMatrix();
+    ofPushMatrix();
 //
-//    flower.enableMaterials();
-//  flower.drawFaces();
+    flower.enableMaterials();
+  flower.drawFaces();
 //
-//    ofPopMatrix();
-//    glDisable(GL_LIGHT0);
-//    glDisable(GL_NORMALIZE);
-//    glDisable(GL_LIGHTING);
+    ofPopMatrix();
+    glDisable(GL_LIGHT0);
+    glDisable(GL_NORMALIZE);
+    glDisable(GL_LIGHTING);
     
-    
-//    pointLight.enable();
-//    pointLight.setSpecularColor( ofFloatColor(1.f, 1.f, 1.f));
-//
-//    pointLight2.enable();
-////    pointLight2.setSpecularColor(ofFloatColor(.8f, .8f, .9f));
-////
-//    pointLight3.enable();
-//    pointLight3.setSpecularColor( ofFloatColor(18.f/255.f,150.f/255.f,135.f/255.f) );
     flower.drawFaces();
 }
